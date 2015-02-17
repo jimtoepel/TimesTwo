b3d1bbd01167876e1d25a41b9cc38b50a09aba47
jimtoepel <jim.toepel@gmail.com>
2015-02-16 20:07:15 -0800

    initial branch

---
+++ TimesTwo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TimesTwo
+//
+//  Created by Jim Toepel on 2/16/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
