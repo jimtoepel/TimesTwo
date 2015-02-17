f51b7c8fc8500837838cdf73b514c38c2e57e090
jimtoepel <jim.toepel@gmail.com>
2015-02-16 21:51:17 -0800

    Pointers and ARC

--- TimesTwo/main.m
+++ TimesTwo/main.m
@@ -12,8 +12,19 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         
-        NSDate * currentTime = nil;
+        NSDate * currentTime = [NSDate date];
         NSLog(@"\ncurrentTime's value is %p", currentTime);
+        
+        NSDate *startTime = currentTime;
+        
+        sleep(2);
+        
+        currentTime = [NSDate date];
+        NSLog(@"\ncurrentTime's value is now %p", currentTime);
+        NSLog(@"\nThe address of the original object is %p", startTime);
+        
+        currentTime = nil;
+        NSLog(@"currentTime's value is %p", currentTime);
         
         NSLog(@"Hello, World!");
     }
